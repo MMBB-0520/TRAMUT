18148b13d25bb19317916ce58cee36401f16d9fa
Regina-123 <reginaky-wm24@student.tarc.edu.my>
2025-12-13 22:00:20 +0800

    gina

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,13 @@ dependencies {
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.foundation:foundation:1.5.4")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ app/src/main/java/com/example/tramut/userInterface/studentTheme/FacilityBook.kt
@@ -0,0 +1,109 @@
+package com.example.tramut.userInterface.studentTheme
+
+import com.example.tramut.R
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.myfacilitybookingsystem.userInterface.studentTheme.MyBookingScreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FacilityBookScreen(
+    selectedTabIndex: Int,
+    onTabSelected: (Int) -> Unit,
+    navController: NavHostController,
+    userId: String,
+    onCITCABClick: () -> Unit,
+    onLibraryABClick: () -> Unit,
+    onSportsABClick: () -> Unit,
+    onCITCTTClick: () -> Unit,
+    onLibraryTTClick: () -> Unit,
+    onSportsTTClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        TabRow(selectedTabIndex = selectedTabIndex) {
+            Tab(
+                selected = selectedTabIndex == 0,
+                onClick = { onTabSelected(0) },
+                text = { Text("Facility Booking") }
+            )
+            Tab(
+                selected = selectedTabIndex == 1,
+                onClick = { onTabSelected(1) },
+                text = { Text("My Bookings") }
+            )
+        }
+        when (selectedTabIndex) {
+            0 -> {
+                FacilityItemUI("Cyber Centre Discussion Room", onCITCABClick, onCITCTTClick)
+                FacilityItemUI("Library Discussion Room", onLibraryABClick, onLibraryTTClick)
+                FacilityItemUI("Sports Facilities", onSportsABClick, onSportsTTClick)
+            }
+            1 -> {
+                MyBookingScreen(userId = userId, navController = navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun FacilityItemUI(name: String, onAddBookingClick: () -> Unit, onViewTTClick: () -> Unit) {
+    Card(
+        Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+
+    ) {
+        Row(
+            Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(name, modifier = Modifier.weight(1f))
+            Icon(
+                painter = painterResource(id = R.drawable.add),
+                contentDescription = "Add",
+                tint = Color.Unspecified,
+                modifier = Modifier
+                    .size(24.dp)
+                    .clickable { onAddBookingClick() }
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Icon(
+                painter = painterResource(id = R.drawable.time),
+                contentDescription = "Time",
+                tint = Color.Unspecified,
+                modifier = Modifier
+                    .size(32.dp)
+                    .clickable { onViewTTClick() }
+            )
+        }
+    }
+}
